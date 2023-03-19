20244eb4c180e830a43f227f0b10c8094c506528
huarui <1145280713@qq.com>
2023-03-19 10:23:57 +0800

    build: 修改打包配置

--- config/database.js
+++ config/database.js
@@ -1,17 +1,18 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'), // host
-      port: env.int('DATABASE_PORT', 3306), // 端口
-      database: env('DATABASE_NAME', 'blog'), // 数据库名
-      user: env('DATABASE_USERNAME', 'root'), // 账号
-      password: env('DATABASE_PASSWORD', 'root'), // 密码
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "localhost"), // host 
+        port: env.int("DATABASE_PORT", 3306), // 端口
+        database: env("DATABASE_NAME", "blog"), // 数据库
+        username: env("DATABASE_USERNAME", "blog"), // 用户名
+        password: env("DATABASE_PASSWORD", "QQyy2007412"), // 密码
       },
+      options: {},
     },
-    debug: false,
   },
 });
 
